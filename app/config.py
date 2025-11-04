af5dd920f315bbf842bdd44ae2749f980879a27d
MCarroll345 <118350383+MCarroll345@users.noreply.github.com>
2025-11-04 13:59:15 +0000

    Added docker files

--- app/config.py
+++ app/config.py
@@ -4,6 +4,6 @@ from pymongo.server_api import ServerApi
 
 config = dotenv_values(".env")
 
-client = MongoClient(config["ATLAS_URI"], server_api=ServerApi('1'))
+client = MongoClient("mongodb+srv://MCarroll123:eebee261202@fitfinder.uzlpzrs.mongodb.net/?retryWrites=true&w=majority&appName=FitFinder", server_api=ServerApi('1'))
 db = client.clothes
 shirts_db = db["shirts"]
