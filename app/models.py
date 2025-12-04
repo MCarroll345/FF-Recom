0616fc1ac7d961f3b53b81276d7afa9c33bd0fa8
MCarroll345 <118350383+MCarroll345@users.noreply.github.com>
2025-12-04 15:16:43 +0000

    Made a file for each clothing type and tested

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
 # app/schemas.py
 from pydantic import BaseModel, EmailStr, constr, conint
 
+#----------------------------------Shirts Models------------------------------------------------
+
 class ShirtClass(BaseModel):
     name:str
     brand:str
@@ -26,6 +28,8 @@ def return_value2(all_shirts,value):
 def all_shirts(all_shirts):
     return [shirt_return(shirt) for shirt in all_shirts]
 
+#----------------------------------Trousers Models------------------------------------------------
+
 class TrouserClass(BaseModel):
     name:str
     brand:str
@@ -50,3 +54,111 @@ def return_value2(all_trousers,value):
 
 def all_trousers(all_trousers):
     return [trouser_return(trouser) for trouser in all_trousers]
+
+#----------------------------------Dress Models------------------------------------------------
+
+class DressClass(BaseModel):
+    name:str
+    brand:str
+    test_value1:int
+    test_value2:int
+    test_value3:int
+
+
+def dress_return(dress):
+    return{
+        "id": str(dress["_id"]),
+        "name": dress["name"],
+        "brand": dress["brand"],
+        "test_value1": dress["test_value1"],
+        "test_value2": dress["test_value2"],
+        "test_value3": dress["test_value3"]
+    }
+
+def return_value2(all_dresses,value):
+    return [dress_return(dress) for dress in all_dresses if (dress.get("test_value2") == value)]
+
+
+def all_dresses(all_dresses):
+    return [dress_return(dress) for dress in all_dresses]
+
+#----------------------------------Jackets Models------------------------------------------------
+
+class JacketClass(BaseModel):
+    name:str
+    brand:str
+    test_value1:int
+    test_value2:int
+    test_value3:int
+
+
+def jacket_return(jacket):
+    return{
+        "id": str(jacket["_id"]),
+        "name": jacket["name"],
+        "brand": jacket["brand"],
+        "test_value1": jacket["test_value1"],
+        "test_value2": jacket["test_value2"],
+        "test_value3": jacket["test_value3"]
+    }
+
+def return_value2(all_jackets,value):
+    return [jacket_return(jacket) for jacket in all_jackets if (jacket.get("test_value2") == value)]
+
+
+def all_jackets(all_jackets):
+    return [jacket_return(jacket) for jacket in all_jackets]
+
+#----------------------------------Shoes Models------------------------------------------------
+
+class ShoeClass(BaseModel):
+    name:str
+    brand:str
+    test_value1:int
+    test_value2:int
+    test_value3:int
+
+
+def shoe_return(shoe):
+    return{
+        "id": str(shoe["_id"]),
+        "name": shoe["name"],
+        "brand": shoe["brand"],
+        "test_value1": shoe["test_value1"],
+        "test_value2": shoe["test_value2"],
+        "test_value3": shoe["test_value3"]
+    }
+
+def return_value2(all_shoes,value):
+    return [shoe_return(shoe) for shoe in all_shoes if (shoe.get("test_value2") == value)]
+
+
+def all_shoes(all_shoes):
+    return [shoe_return(shoe) for shoe in all_shoes]
+
+#----------------------------------Skirts Models------------------------------------------------
+
+class SkirtClass(BaseModel):
+    name:str
+    brand:str
+    test_value1:int
+    test_value2:int
+    test_value3:int
+
+
+def skirt_return(skirt):
+    return{
+        "id": str(skirt["_id"]),
+        "name": skirt["name"],
+        "brand": skirt["brand"],
+        "test_value1": skirt["test_value1"],
+        "test_value2": skirt["test_value2"],
+        "test_value3": skirt["test_value3"]
+    }
+
+def return_value2(all_skirts,value):
+    return [skirt_return(skirt) for skirt in all_skirts if (skirt.get("test_value2") == value)]
+
+
+def all_skirts(all_skirts):
+    return [skirt_return(skirt) for skirt in all_skirts]
